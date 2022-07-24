3e9e61b7eb14f1591d1b0a2e985917975af79db2
yunji <zion830@naver.com>
2022-07-24 16:46:51 +0900

    refactor: WapleDataStore에서 dataStore private로 변경

--- app/src/main/java/com/euphony/waple/data/WapleDataStore.kt
+++ app/src/main/java/com/euphony/waple/data/WapleDataStore.kt
@@ -13,7 +13,7 @@ object WapleDataStore {
     const val KEY_PIN: String = "key_pin"
     const val KEY_WIFI_INFO: String = "key_wifi_info"
 
-    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(DATASTORE_NAME)
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(DATASTORE_NAME)
 
     val adapter = LocalDataStore(dataStore = WapleApplication.getContext().dataStore)
 }
