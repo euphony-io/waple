cd81f3d7c9b536148050b20241b6a1fe07c22ba0
yunji <zion830@naver.com>
2022-07-26 19:50:42 +0900

    feat: 와이파이 등록 화면 추가(#12)

--- app/src/main/java/com/euphony/waple/add_wifi/ui/AddWifiScreen.kt
+++ app/src/main/java/com/euphony/waple/add_wifi/ui/AddWifiScreen.kt
@@ -1,31 +1,41 @@
 package com.euphony.waple.add_wifi.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.euphony.waple.R
-import com.euphony.waple.add_wifi.AddWifiViewModel
-import com.euphony.waple.ui.component.HomeButton
-import com.euphony.waple.ui.theme.Yellow
+import com.euphony.waple.ui.component.BasicDivider
+import com.euphony.waple.ui.component.RectButton
+import com.euphony.waple.ui.component.TitleText
 
+@Preview
 @Composable
 fun AddWifiScreen() {
-    val viewModel = AddWifiViewModel()
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .padding(horizontal = 24.dp)
+            .verticalScroll(scrollState)
     ) {
-        HomeButton(
-            onClick = { viewModel.speak("test hello hello") },
-            backgroundColor = Yellow,
-            text = stringResource(id = R.string.add_wifi)
+        TitleText(titleResId = R.string.add_wifi)
+        BasicDivider()
+        Row() {
+
+        }
+        BasicDivider()
+        RectButton(
+            onClick = { /*TODO*/ },
+            text = stringResource(id = R.string.finish)
         )
     }
 }
