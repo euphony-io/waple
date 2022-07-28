463335f4be14910dbfb3a5306385b54df28fb86e
SANGHYUN KIM <haan823@naver.com>
2022-07-28 11:25:34 +0900

    feat: 와이파이 검색중 화면 개발

--- app/src/main/java/com/euphony/waple/find_wifi/ui/FindWifiScreen.kt
+++ app/src/main/java/com/euphony/waple/find_wifi/ui/FindWifiScreen.kt
@@ -1,7 +1,57 @@
 package com.euphony.waple.find_wifi.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.euphony.waple.R
+import com.euphony.waple.Screen
+import com.euphony.waple.find_wifi.FindWifiViewModel
+import com.euphony.waple.ui.theme.Yellow
 
 @Composable
-fun FindWifiScreen() {
+fun FindWifiScreen(
+    startScreenBtnClick: (Screen) -> Unit,
+) {
+    val viewModel = FindWifiViewModel()
+    val listenResult by viewModel.listenResult.observeAsState("")
+    viewModel.listen()
+    if (!listenResult.isEmpty()) {
+        println("result is not empty")
+        viewModel.finish()
+        startScreenBtnClick(Screen.HomeScreen)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = stringResource(id = R.string.look_for_wifi),
+            color = Yellow,
+            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp
+        )
+        Image(
+            modifier = Modifier
+                .width(80.dp)
+                .height(80.dp),
+            painter = painterResource(id = R.drawable.waffle2),
+            contentDescription = "waffle2"
+        )
+    }
 }
