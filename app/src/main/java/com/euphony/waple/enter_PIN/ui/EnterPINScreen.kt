8adced1138cd5c06038afd2d3a5d7835670dc248
kimhaedam <haedam615@naver.com>
2022-07-29 17:11:52 +0900

    fix: textfield에서 값 입력 받은 후 화면 전환을 위한 확인 버튼 수정 필요

--- app/src/main/java/com/euphony/waple/enter_PIN/ui/EnterPINScreen.kt
+++ app/src/main/java/com/euphony/waple/enter_PIN/ui/EnterPINScreen.kt
@@ -2,78 +2,56 @@ package com.euphony.waple.enter_PIN.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.euphony.waple.Screen
 import com.euphony.waple.ui.component.HomeButton
 import com.euphony.waple.ui.theme.Yellow
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
-import com.euphony.waple.ui.theme.PurpleGrey40
 
-@Preview
 @Composable
 fun EnterPINScreen(
-    startScreenBtnClick: Screen.WifiListScreen,
+    startScreenBtnClick: String,
 ) {
     Column(
-        modifier = Modifier
+        modifier = androidx.compose.ui.Modifier
             .fillMaxSize()
             .background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            modifier = Modifier.padding(top = 8.dp, bottom = 48.dp),
+            modifier = androidx.compose.ui.Modifier.padding(top = 20.dp, bottom = 50.dp),
             text = "Wi-Fi 사용 권한을 얻으려면 PIN 번호를 입력해주세요!",
             color = Color.Black,
             fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
+            fontSize = 16.sp
         )
 
-        @Composable
-        fun TextFieldTest() {
-            var pinInput by remember { mutableStateOf(TectFieldValue()) }
-
-            Column(
-                Modifier.padding(16.dp)
-                    .fillMaxWidth()
-            ) {
-
-                OutlinedTextField(
-                    modifier = java.lang.reflect.Modifier.fillMaxWidth(),
-                    value = pinInput,
-                    singleLine = true,
-                    maxLines = 1,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    onValueChange = { newValue -> pinInput = newValue },
-                    label = "PIN 번호",
-                    placeHolder = "ex)1234"
-                )
-            }
-        }
+        OutlinedTextField(
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            Spacer(modifier= Modifier.height(16.dp)),
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = "PIN 번호",
+            placeHolder = "ex)1234"
+        )
 
         HomeButton(
-            onClick = { startScreenBtnClick(Screen.WifiListScreen) },
+            onClick = {startScreenBtnClick()},
             backgroundColor = Yellow,
             text = "확인"
         )
     }
 }
-
 
 
 
